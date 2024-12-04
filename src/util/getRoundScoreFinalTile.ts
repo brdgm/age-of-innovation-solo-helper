1fdcbe817bcd5b42420f59fa6d19c58a4f5e5715
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-12-04 18:12:49 +0100

    fix cases

--- src/util/getRoundScoreFinalTile.ts
+++ src/util/getRoundScoreFinalTile.ts
@@ -8,9 +8,9 @@ import ScienceDiscipline from '@/services/enum/ScienceDiscipline'
 export default function getRoundScoreFinalTile(roundScoreFinalTile: number) : RoundScoreFinalTile {
   switch (roundScoreFinalTile) {
     case 1: return { vpRound6: 8, scienceDiscipline: ScienceDiscipline.MEDICINE }
-    case 1: return { vpRound6: 6, scienceDiscipline: ScienceDiscipline.LAW }
-    case 1: return { vpRound6: 7, scienceDiscipline: ScienceDiscipline.ENGINEERING }
-    case 1: return { vpRound6: 7, scienceDiscipline: ScienceDiscipline.BANKING }
+    case 2: return { vpRound6: 6, scienceDiscipline: ScienceDiscipline.LAW }
+    case 3: return { vpRound6: 7, scienceDiscipline: ScienceDiscipline.ENGINEERING }
+    case 4: return { vpRound6: 7, scienceDiscipline: ScienceDiscipline.BANKING }
     default:
       throw new Error(`Invalid round score tile: ${roundScoreFinalTile}`)
   }
