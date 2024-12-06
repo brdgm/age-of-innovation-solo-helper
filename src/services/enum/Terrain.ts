ab3e0feae15839b82e8856f1ec5d44e731bdc0cc
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-12-06 10:18:26 +0100

    Integrate Automa Train Priorty (ATP) cards

---
+++ src/services/enum/Terrain.ts
@@ -0,0 +1,13 @@
+/**
+ * Terrain
+ */
+enum Terrain {
+  DESERT = 'desert',
+  FOREST = 'forest',
+  LAKE = 'lake',
+  MOUNTAIN = 'mountain',
+  PLAINS = 'plains',
+  SWAMP = 'swamp',
+  WASTELAND = 'wasteland'
+}
+export default Terrain
